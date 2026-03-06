f6db71d9947a3b5ec537660130c6dbf901e082b3
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-06 20:38:24 -0300

    updating imports from modules e package

--- src/div_fem/__init__.py
+++ src/div_fem/__init__.py
@@ -1,5 +1,6 @@
 from .matrices import Matrix
+from .fem_analysis import GlobalStiffnessMatrix
 
 __version__ = "0.1.0"
 
-__all__ = ["Matrix"]
+__all__ = ["Matrix", "GlobalStiffnessMatrix"]
---
+++ src/div_fem/fem_analysis/__init__.py
@@ -0,0 +1,3 @@
+from .global_entities import GlobalStiffnessMatrix
+
+__all__ = ["GlobalStiffnessMatrix"]
