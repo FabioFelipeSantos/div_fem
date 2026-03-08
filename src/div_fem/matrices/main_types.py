346bd21adc483c6c421879cd107706226a2f734a
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-08 19:03:34 -0300

    creating a reusable types for vectors and matrices data

---
+++ src/div_fem/matrices/main_types.py
@@ -0,0 +1,6 @@
+from typing import Union
+
+_VectorDataType = Union[list[float], list[int], list[Union[float, int]]]
+_MatrixDataType = Union[
+    list[list[float]], list[list[int]], list[list[Union[float, int]]]
+]
