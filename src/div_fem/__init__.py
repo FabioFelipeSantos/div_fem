dbd454c40455787c2fb083290d681c7b2916416c
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-10 11:41:22 -0300

    making the Point class default for import from div_fem package

--- src/div_fem/__init__.py
+++ src/div_fem/__init__.py
@@ -4,6 +4,7 @@ from .fem_analysis import (
     GlobalForcesVector,
     LocalForcesVector,
     LocalStiffnessMatrix,
+    Point,
 )
 
 __version__ = "0.1.0"
@@ -15,4 +16,5 @@ __all__ = [
     "GlobalForcesVector",
     "LocalForcesVector",
     "LocalStiffnessMatrix",
+    "Point",
 ]
--- src/div_fem/fem_analysis/__init__.py
+++ src/div_fem/fem_analysis/__init__.py
@@ -1,9 +1,11 @@
 from .global_entities import GlobalStiffnessMatrix, GlobalForcesVector
 from .local_entities import LocalForcesVector, LocalStiffnessMatrix
+from .geometry import Point
 
 __all__ = [
     "GlobalStiffnessMatrix",
     "GlobalForcesVector",
     "LocalForcesVector",
     "LocalStiffnessMatrix",
+    "Point",
 ]
