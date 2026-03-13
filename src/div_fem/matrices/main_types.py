1ccf829552bce86879c7abe442de6383163e1738
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-13 12:40:21 -0300

    adding Point as input for vectors in the Vector class

--- src/div_fem/matrices/main_types.py
+++ src/div_fem/matrices/main_types.py
@@ -1,7 +1,9 @@
 from typing import Sequence
 
+from div_fem.fem_analysis.geometry.point import Point
+
 _VectorDataType = list[float]
 _MatrixDataType = list[list[float]]
 
-_VectorInputType = Sequence[float]
+_VectorInputType = Sequence[float] | Point
 _MatrixInputType = Sequence[Sequence[float]]
