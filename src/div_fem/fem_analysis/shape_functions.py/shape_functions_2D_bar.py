5c2b208ec267519cedfa30cb457d8e212e7c9460
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-13 12:44:47 -0300

    implementing the shape_functions submodule and 2D shape functions for bars

---
+++ src/div_fem/fem_analysis/shape_functions.py/shape_functions_2D_bar.py
@@ -0,0 +1,15 @@
+from div_fem.matrices.base_matrix import Matrix
+
+
+def shape_functions_2D_bar(zeta: float) -> Matrix:
+    N1 = 1 - zeta
+    N2 = zeta
+
+    return Matrix([[N1, N2]])
+
+
+def first_derivative_shape_functions_2D_bar(zeta) -> Matrix:
+    dN1 = -1
+    dN2 = 1
+
+    return Matrix([[dN1, dN2]])
