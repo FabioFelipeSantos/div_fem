0be947d9568b156ec7a50246bc9c83e5addfc001
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-13 12:39:20 -0300

    removing imports from __init__ to remove circular imports. In the future this will be correctly fixed

--- src/div_fem/fem_analysis/global_entities/__init__.py
+++
@@ -1,4 +0,0 @@
-from .stiffness_matrix import GlobalStiffnessMatrix
-from .forces_vector import GlobalForcesVector
-
-__all__ = ["GlobalStiffnessMatrix", "GlobalForcesVector"]
--- src/div_fem/fem_analysis/local_entities/__init__.py
+++
@@ -1,4 +0,0 @@
-from .local_forces_vector import LocalForcesVector
-from .local_stiffness_matrix import LocalStiffnessMatrix
-
-__all__ = ["LocalForcesVector", "LocalStiffnessMatrix"]
