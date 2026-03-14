9013a06cf2dd7ca65cf2dc8a3c18c2f0217b39da
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-14 16:06:31 -0300

    changing from zeta to xi independent variable name and fixing the derivative value

--- src/div_fem/fem_analysis/shape_functions/shape_functions_2D_bar.py
+++ src/div_fem/fem_analysis/shape_functions/shape_functions_2D_bar.py
@@ -1,15 +1,15 @@
 from div_fem.matrices.base_matrix import Matrix
 
 
-def shape_functions_2D_bar(zeta: float) -> Matrix:
-    N1 = 1 - zeta
-    N2 = zeta
+def shape_functions_2D_bar(xi: float) -> Matrix:
+    N1 = (1 - xi) / 2
+    N2 = (1 + xi) / 2
 
     return Matrix([[N1, N2]])
 
 
-def first_derivative_shape_functions_2D_bar(zeta: float) -> Matrix:
-    dN1 = -1
-    dN2 = 1
+def first_derivative_shape_functions_2D_bar(xi: float) -> Matrix:
+    dN1 = -1 / 2
+    dN2 = 1 / 2
 
     return Matrix([[dN1, dN2]])
