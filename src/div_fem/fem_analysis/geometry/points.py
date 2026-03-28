5ee89c12e77e6397c2d1e5b81508de90dafa3bed
Fabio Santos <fabiotrabmat@gmail.com>
2026-03-28 15:46:40 -0300

    modifying the Point class to pass the degree of freedom to Elements container

--- src/div_fem/fem_analysis/geometry/points.py
+++ src/div_fem/fem_analysis/geometry/points.py
@@ -15,32 +15,22 @@ class Points:
     _instance: Points | None = None
 
     _points: list[Point] = []
-    _degrees_of_freedom: list[list[int]] = []
 
-    _index_next_point: int = 1
-
-    _number_of_degree_of_freedom_per_node: int
-    _range_number_dof: range
-
-    def __new__(cls, number_of_degree_of_freedom_per_node: int) -> Self:
+    def __new__(cls) -> Self:
         if cls._instance:
             raise ValueError("The Points object must be single per analysis.")
 
         cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, number_of_degree_of_freedom_per_node: int) -> None:
+    def __init__(self) -> None:
         if self._initialized:
             return
 
         self._initialized = True
-        self._number_of_degree_of_freedom_per_node = (
-            number_of_degree_of_freedom_per_node
-        )
-        self._range_number_dof = range(self._number_of_degree_of_freedom_per_node)
 
     @overload
-    def __call__(self, element: int) -> Point:
+    def __call__(self, elements: int) -> Point:
         """
         Calling the Points class with the element as int value will return the point associated at that int nodal index. The indexation is based on 0 array index Python, but will must provide the nodal index wanted in normal indexation.
 
@@ -50,7 +40,7 @@ class Points:
         pass
 
     @overload
-    def __call__(self, element: Point) -> None:
+    def __call__(self, elements: Point | list[Point]) -> None:
         """
         Calling the Points class with the element as Point instance will add the point to the class and will calculate your degrees of freedom list based in the value of number of degree of freedom per node, as declared when Points was instantiated.
 
@@ -59,51 +49,31 @@ class Points:
         """
         pass
 
-    def __call__(self, element: int | Point) -> Point | None:
-        if isinstance(element, int):
-            return self.point(element)
-
-        if isinstance(element, Point):
-            self.add(element)
+    def __call__(self, elements: int | Point | list[Point]) -> Point | None:
+        if isinstance(elements, int):
+            return self.point(elements)
+        else:
+            self.add(elements)
             return
 
     @property
     def number_of_points(self) -> int:
-        return self._index_next_point - 1
-
-    @property
-    def degrees_of_freedom(self) -> list[list[int]]:
-        return self._degrees_of_freedom
-
-    def degree_of_freedom(self, nodal_index: int) -> list[int]:
-        return self._degrees_of_freedom[nodal_index - 1]
+        return len(self._points)
 
     def point(self, nodal_index: int) -> Point:
         return self._points[nodal_index - 1]
 
-    def point_info(self, nodal_index: int) -> PointInfo:
-        return PointInfo(
-            nodal_index - 1,
-            self.point(nodal_index),
-            self.degree_of_freedom(nodal_index),
-        )
+    def add(self, points: Point | list[Point]) -> None:
+        if isinstance(points, list):
+            for point in points:
+                self._points.append(point)
+        else:
+            self._points.append(points)
 
-    def add(self, point: Point) -> None:
-        self._points.append(point)
-
-        self._degrees_of_freedom.append(
-            [
-                self._number_of_degree_of_freedom_per_node
-                * (self._index_next_point - 1)
-                + dof_index
-                for dof_index in self._range_number_dof
-            ]
-        )
-        self._index_next_point += 1
         return
 
     def __str__(self) -> str:
-        if self._index_next_point == 1:
+        if len(self._points) == 1:
             return "Points()"
 
         begin = "Points(\n    "
