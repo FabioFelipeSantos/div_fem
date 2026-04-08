b2b907778c5e8e8da626edb13c5c61b8b3dd85ba
Fabio Santos <fabiotrabmat@gmail.com>
2026-04-08 19:09:14 -0300

    changing attributes to include descriptors to them; removing properties that was changed to attributes

--- src/div_fem/fem_analysis/geometry/elements_interface.py
+++ src/div_fem/fem_analysis/geometry/elements_interface.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Generic, Mapping, Sequence, TypeVar
+from typing import TYPE_CHECKING, Any, Generic, Mapping, TypeVar
 
 from div_fem.fem_analysis.geometry.elements.element_load_interface import (
     ElementLoadInterface,
@@ -10,19 +10,22 @@ from div_fem.fem_analysis.geometry.elements.shape_functions_interface import (
 from div_fem.fem_analysis.geometry.point import Point
 from div_fem.matrices.base_matrix import Matrix
 from div_fem.matrices.base_vector import Vector
+from div_fem.utils.descriptors.descriptor_private_name import DescriptorBaseClass
 
 if TYPE_CHECKING:
     from div_fem.fem_analysis.geometry.elements_container import Elements
 
-_GeometryProperties = TypeVar(
-    "_GeometryProperties", bound=Mapping[str, Any], covariant=True
-)
-_MaterialAndSectionProperties = TypeVar(
-    "_MaterialAndSectionProperties", bound=Mapping[str, Any], covariant=True
-)
+_GeometryProperties = TypeVar("_GeometryProperties", bound=Mapping[str, Any], covariant=True)
+_MaterialAndSectionProperties = TypeVar("_MaterialAndSectionProperties", bound=Mapping[str, Any], covariant=True)
 _ElementType = TypeVar("_ElementType", bound=str, covariant=True)
-_ShapeFunctions = TypeVar("_ShapeFunctions", bound=ShapeFunctions, covariant=True)
 _ElementLoad = TypeVar("_ElementLoad", bound=ElementLoadInterface, covariant=True)
+
+
+def index_validation(value: int) -> None:
+    if value < 0:
+        raise ValueError(f"A index for the element must be greater than 0. Received {value}")
+
+    return
 
 
 class ElementInterface(
@@ -31,20 +34,23 @@ class ElementInterface(
         _ElementType,
         _GeometryProperties,
         _MaterialAndSectionProperties,
-        _ShapeFunctions,
         _ElementLoad,
     ],
 ):
-    _type: _ElementType
+    _extreme_points: tuple[Point, Point]
     _points: list[Point]
-    _shape_functions: _ShapeFunctions
     _geometry_properties: _GeometryProperties
     _material_and_section_properties: _MaterialAndSectionProperties
     _loads: list[_ElementLoad] | None
 
-    @property
-    @abstractmethod
-    def shape_functions(self) -> ShapeFunctions: ...
+    type = DescriptorBaseClass[_ElementType]()
+    number_interpolation_points = DescriptorBaseClass[int]()
+    interpolation_points_already_put_in_points_class: bool
+
+    elements_container = DescriptorBaseClass["Elements"]()
+    index = DescriptorBaseClass[int](validation=index_validation)
+    total_degree_of_freedom = DescriptorBaseClass[int]()
+    shape_functions = DescriptorBaseClass[ShapeFunctions]()
 
     @property
     @abstractmethod
@@ -64,32 +70,15 @@ class ElementInterface(
 
     @property
     @abstractmethod
-    def type(self) -> _ElementType: ...
-
-    @property
-    @abstractmethod
     def geometry_properties(self) -> _GeometryProperties: ...
 
     @property
     @abstractmethod
-    def container(self) -> Elements: ...
+    def interpolation_points(self) -> list[Point]: ...
 
     @property
     @abstractmethod
-    def index(self) -> int: ...
-
-    @abstractmethod
-    def set_elements_container(self, container: Elements) -> None: ...
-
-    @abstractmethod
-    def set_element_index(self, index: int) -> None: ...
-
-    @property
-    @abstractmethod
-    def points(self) -> list[Point]: ...
-
-    def __len__(self) -> int:
-        return len(self._points)
+    def extreme_points(self) -> tuple[Point, Point]: ...
 
     def print(self, idx: int | None = None) -> None:
         """
@@ -101,30 +90,32 @@ class ElementInterface(
             print(f"\n{self[idx]}\n")
 
     def __str__(self) -> str:
-        begin = f"Element[{self.index:>3d}]:(\n  "
+        begin = f"Element(\n  "
         element_data: list[str] = []
 
-        for idx, point in enumerate(self._points):
-            element_data.append(f"{idx + 1}: {repr(point)}")
+        for idx, point in enumerate(self._extreme_points):
+            dof_numbers: list[int] | None = getattr(point, "dof_numbers", None)
+            if not dof_numbers:
+                element_data.append(f"{idx + 1}: {repr(point)}")
+            else:
+                element_data.append(f"{idx + 1}: {repr(point)}, DOF: {dof_numbers}")
 
-        element_string = ",  ".join(element_data)
+        element_string = ";  ".join(element_data)
 
-        end = f"\n  Number of interpolation points = {self.container.number_of_interpolation_points}\n  DOF = {self.container.dof_string(self.index)}\n)"
+        end = f"\n  Number of interpolation points = {self.number_interpolation_points}\n)"
         return begin + element_string + end
 
     def __repr__(self) -> str:
-        return f"Element({", ".join([repr(point) for point in self._points])})"
+        return f"Element({", ".join([repr(point) for point in self._extreme_points])})"
 
-    def __getitem__(self, idx: int) -> tuple[Point, int | list[int]]:
+    def __getitem__(self, idx: int) -> tuple[Point, list[int]]:
         """
         The index is the point number, starting at 0, used in the list to create this element.
 
         Returns:
             tuple(Point, int | list[int]): A tuple with the point and the degree of freedom of the point
         """
-        if idx > self.container.number_of_interpolation_points - 1:
-            raise IndexError(
-                f"The element doesn't have more than {self.container.number_of_interpolation_points} points. The index count starts at zero."
-            )
+        if idx > self.number_interpolation_points - 1:
+            raise IndexError(f"The element doesn't have more than {self.number_interpolation_points} points. The index count starts at zero.")
 
-        return (self._points[idx], self.container.degree_of_freedom(idx))
+        return (self._extreme_points[idx], self._extreme_points[idx].dof_numbers)
