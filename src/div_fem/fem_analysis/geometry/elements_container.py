f5c8bfd04e24a22da59ee8eafeea8736e12b9997
Fabio Santos <fabiotrabmat@gmail.com>
2026-04-08 19:11:00 -0300

    adding structural analysis as attribute and removing and fixing other methods to satisfy the new features

--- src/div_fem/fem_analysis/geometry/elements_container.py
+++ src/div_fem/fem_analysis/geometry/elements_container.py
@@ -4,10 +4,9 @@ from typing import TYPE_CHECKING, Any, Mapping, Self, Union, overload
 from div_fem.fem_analysis.geometry.elements.element_load_interface import (
     ElementLoadInterface,
 )
-from div_fem.fem_analysis.geometry.elements.shape_functions_interface import (
-    ShapeFunctions,
-)
 from div_fem.fem_analysis.geometry.elements_interface import ElementInterface
+from div_fem.fem_analysis.structural_analysis.structural_analysis_interface import StructuralAnalysisInterface
+from div_fem.utils.descriptors.descriptor_private_name import DescriptorBaseClass
 
 if TYPE_CHECKING:
     from div_fem.fem_analysis.geometry.point import Point
@@ -16,7 +15,6 @@ _ContainerElement = ElementInterface[
     str,
     Mapping[str, Any],
     Mapping[str, Any],
-    ShapeFunctions,
     ElementLoadInterface[
         str,
         Any,
@@ -26,24 +24,36 @@ _ContainerElement = ElementInterface[
 ]
 
 
+class StructuralAnalysisDescriptor(DescriptorBaseClass[StructuralAnalysisInterface]):
+
+    def __set__(self, obj: "Elements", value: StructuralAnalysisInterface) -> None:
+        super().__set__(obj, value)
+
+        try:
+            points_class = value.points
+
+            for element in obj:
+                if not element.interpolation_points_already_put_in_points_class:
+                    points_class(element.interpolation_points)
+                    element.interpolation_points_already_put_in_points_class = True
+        except:
+            raise
+
+
 class Elements:
     _initialized: bool = False
     _instance: Elements | None = None
 
-    _elements: list[_ContainerElement] = []
+    structural_analysis = StructuralAnalysisDescriptor()
 
-    _degrees_of_freedom: list[list[int]] = []
+    _elements: list[_ContainerElement] = []
 
     _index_next_element: int = 1
 
     _number_of_interpolation_points: int
-    _number_of_degree_of_freedom_per_node: int
-    _range_number_dof: range
 
     def __new__(
         cls,
-        number_of_interpolation_points: int,
-        number_of_degree_of_freedom_per_element: int,
     ) -> Self:
         if cls._instance:
             raise ValueError("The Elements object must be single per analysis.")
@@ -53,54 +63,23 @@ class Elements:
 
     def __init__(
         self,
-        number_of_interpolation_points: int,
-        number_of_degree_of_freedom_per_node: int,
     ) -> None:
         if self._initialized:
             return
 
         self._initialized = True
-        self._number_of_interpolation_points = number_of_interpolation_points
-        self._number_of_degree_of_freedom_per_node = (
-            number_of_degree_of_freedom_per_node
-        )
-        self._range_number_dof = range(self._number_of_degree_of_freedom_per_node)
 
     @property
     def number_of_elements(self) -> int:
-        return len(self._elements)
-
-    @property
-    def number_of_interpolation_points(self) -> int:
-        """The number of points used in interpolation functions this element."""
-        return self._number_of_interpolation_points
-
-    @property
-    def total_degree_of_freedom(self) -> int:
-        """The number of total degree of freedom of the element. Useful for making the element stiffness matrix and forces vector."""
-        return (
-            self._number_of_interpolation_points
-            * self._number_of_degree_of_freedom_per_node
-        )
-
-    @property
-    def degrees_of_freedom(self) -> list[list[int]]:
-        return self._degrees_of_freedom
-
-    def degree_of_freedom(self, element_index: int) -> list[int]:
-        return self._degrees_of_freedom[element_index - 1]
+        return self._index_next_element - 1
 
     @overload
     def __call__(self, elements_or_index: int) -> _ContainerElement: ...
 
     @overload
-    def __call__(
-        self, elements_or_index: _ContainerElement | list[_ContainerElement]
-    ) -> None: ...
+    def __call__(self, elements_or_index: _ContainerElement | list[_ContainerElement]) -> None: ...
 
-    def __call__(
-        self, elements_or_index: int | _ContainerElement | list[_ContainerElement]
-    ) -> _ContainerElement | None:
+    def __call__(self, elements_or_index: int | _ContainerElement | list[_ContainerElement]) -> _ContainerElement | None:
         if isinstance(elements_or_index, int):
             return self._elements[elements_or_index - 1]
         else:
@@ -116,50 +95,44 @@ class Elements:
         return
 
     def _adding_one_element(self, element: _ContainerElement) -> None:
-        element.set_elements_container(self)
-        element.set_element_index(self._index_next_element)
+        element.elements_container = self
+        element.index = self._index_next_element
         self._elements.append(element)
-        self._degrees_of_freedom.append(self._calculate_degree_of_freedom(len(element)))
         self._index_next_element += 1
-
-    def _calculate_degree_of_freedom(self, number_of_points: int) -> list[int]:
-        """This function will must be moved to the element and here is just a reference for linear 2D elements"""
-
-        dof: list[int] = []
-
-        element_number = self._index_next_element
-        number_interpolation = self._number_of_interpolation_points
-        r = range(
-            number_interpolation * (element_number - 1),
-            number_interpolation * element_number,
-        )
-
-        for n in r:
-            for d in self._range_number_dof:
-                dof.append(self._number_of_degree_of_freedom_per_node * n + d)
-
-        return dof
-
-    def dof_string(self, index: int) -> str:
-        element_dof = self._degrees_of_freedom[index - 1]
-        n = self._number_of_degree_of_freedom_per_node
-        data = [element_dof[n * i : n * (i + 1)] for i in range(n)]
-        return str(data)
 
     def print(self) -> None:
         print(str(self))
         return
 
     def _string_for_element_info(self, element: _ContainerElement) -> str:
-        begin = f"Element[{element.index:>3d}]:(\n    "
+        begin = f"Element[{element.index:>3d}]: (\n    "
         element_data: list[str] = []
 
-        for idx, point in enumerate(element.points):
-            element_data.append(f"{idx + 1}: {repr(point)}")
+        extreme_points = element.extreme_points
+
+        try:
+            interpolation_points = element.interpolation_points
+
+            if len(interpolation_points) > 2:
+                element_data = [
+                    repr(extreme_points[0]),
+                    repr(interpolation_points[0]),
+                    " ... ",
+                    repr(interpolation_points[-1]),
+                    repr(extreme_points[1]),
+                ]
+            else:
+                element_data.append(repr(extreme_points[0]))
+                for point in interpolation_points:
+                    element_data.append(f"{repr(point)}")
+                element_data.append(repr(extreme_points[1]))
+        except:
+            for point in extreme_points:
+                element_data.append(f"{repr(point)}")
 
         element_string = ",  ".join(element_data)
 
-        end = f"\n    Number of interpolation points = {self._number_of_interpolation_points}\n    DOF = {self.dof_string(element.index)}\n  )"
+        end = f"\n  )"
         return begin + element_string + end
 
     def __str__(self) -> str:
@@ -168,26 +141,12 @@ class Elements:
 
         begin = "Elements(\n  "
 
-        middle = ",\n  ".join(
-            [f"{self._string_for_element_info(element)}" for element in self._elements]
-        )
+        middle = ",\n  ".join([f"{self._string_for_element_info(element)}" for element in self._elements])
         end = "\n)"
         return begin + middle + end
 
     def __getitem__(self, element_index: int) -> _ContainerElement:
-        if element_index < 0:
-            raise IndexError(
-                "There's not negative indexation for the Elements container."
-            )
-
-        if element_index == 0:
-            raise IndexError(
-                "The element number must be usual non 0 indexation. Just inside the class that 0 indexation is used."
-            )
-
         if element_index > self._index_next_element - 1:
-            raise IndexError(
-                f"The element index is greater than the number of elements {self._index_next_element -1}"
-            )
+            raise IndexError(f"The element index is greater than the number of elements {self._index_next_element -1}")
 
-        return self._elements[element_index - 1]
+        return self._elements[element_index]
