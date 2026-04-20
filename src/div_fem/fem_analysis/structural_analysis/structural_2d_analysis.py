648e405dc5b40bf1382b159015d9bab6e16f8d7e
Fabio Santos <fabiotrabmat@gmail.com>
2026-04-20 20:27:39 -0300

    feat: Implement renumbering of degrees of freedom and bandwidth calculation in Structural2DAnalysis

--- src/div_fem/fem_analysis/structural_analysis/structural_2d_analysis.py
+++ src/div_fem/fem_analysis/structural_analysis/structural_2d_analysis.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any, Self
+from typing import Any
 
 
 from div_fem.fem_analysis.structural_analysis.structural_analysis_interface import (
@@ -8,14 +8,33 @@ from div_fem.fem_analysis.structural_analysis.structural_analysis_interface impo
 
 
 class Structural2DAnalysis(StructuralAnalysisInterface):
+    max_bandwidth: int
+    total_degree_of_freedom: int
+    global_stiffness_matrix: None
 
     def __call__(self) -> Any:
-        dof = self.dof
-        points = self.points
-        elements = self.elements
-        boundaries_cond = self.boundaries_cond
+        self.total_degree_of_freedom = self._total_dof_calculation()
+        self.renumber_dofs()
+        self.max_bandwidth = self._bandwidth_calculation()
 
-        print(dof)
-        print(points)
-        print(elements)
-        print(boundaries_cond)
+    def renumber_dofs(self) -> None:
+        new_order = self.elements.reverse_cuthill_mckee()
+
+        for new_idx, old_node_index in enumerate(new_order):
+            point = self.points(old_node_index)
+            point.dof_numbers = self.points.calculating_dof_numbers(new_idx)
+
+    def _bandwidth_calculation(self) -> int:
+        all_elements_bandwidths: list[int] = []
+
+        for element in self.elements:
+            element_dof = element.degree_of_freedom
+            all_elements_bandwidths.append(max(element_dof) - min(element_dof))
+
+        return max(all_elements_bandwidths)
+
+    def _total_dof_calculation(self) -> int:
+        number_of_points = self.points.number_of_points
+        number_of_dof_per_node = self.points.dof_per_node
+
+        return number_of_points * number_of_dof_per_node
--- src/div_fem/fem_analysis/structural_analysis/structural_analysis_interface.py
+++ src/div_fem/fem_analysis/structural_analysis/structural_analysis_interface.py
@@ -13,12 +13,9 @@ if TYPE_CHECKING:
     from div_fem.fem_analysis.geometry.boundaries_conditions import BoundariesConditions
 
 
-class StructuralPoints(PrivateName):
+class PointsDescriptor(DescriptorBaseClass["Points"]):
 
-    def __get__(self, obj: StructuralAnalysisInterface, objtype=None) -> Points:
-        return getattr(obj, self.private_name)
-
-    def __set__(self, obj: StructuralAnalysisInterface, value: Points) -> None:
+    def __set__(self, obj: StructuralAnalysisInterface, value) -> None:
         if value.number_of_points < 2:
             raise ValueError(
                 "Expected that number of points be greater than zero since any 2D element must have a starting and ending points"
@@ -35,45 +32,29 @@ class StructuralElements(PrivateName):
 
     def __set__(self, obj: StructuralAnalysisInterface, value: Elements) -> None:
         if value.number_of_elements < 1:
-            raise ValueError(
-                f"Expected one or more elements. Received {value.number_of_elements}"
-            )
+            raise ValueError(f"Expected one or more elements. Received {value.number_of_elements}")
 
         setattr(obj, self.private_name, value)
         value.structural_analysis = obj
 
 
-class StructuralBoundaryConditions(PrivateName):
-
-    def __get__(
-        self, obj: StructuralAnalysisInterface, objtype=None
-    ) -> BoundariesConditions:
-        return getattr(obj, self.private_name)
-
-    def __set__(
-        self, obj: StructuralAnalysisInterface, value: BoundariesConditions
-    ) -> None:
-        if value.count < 1:
-            raise ValueError(
-                f"Expected one or more boundaries conditions. Received {value.count}"
-            )
-
-        setattr(obj, self.private_name, value)
+def _boundary_conditions_validation(value: BoundariesConditions) -> None:
+    if value.count < 1:
+        raise ValueError(f"Expected one or more boundaries conditions. Received {value.count}")
 
 
 class StructuralAnalysisInterface:
     _initialized: bool = False
     _instance: StructuralAnalysisInterface | None = None
 
-    points = StructuralPoints()
+    dof_per_node = DescriptorBaseClass[int]()
+    points = PointsDescriptor()
     elements = StructuralElements()
-    boundaries_cond = StructuralBoundaryConditions()
+    boundaries_cond = DescriptorBaseClass["BoundariesConditions"](validation=_boundary_conditions_validation)
 
     def __new__(cls) -> Self:
         if cls._instance:
-            raise ValueError(
-                "The StructuralAnalysisInterface class is single to the analysis."
-            )
+            raise ValueError("The StructuralAnalysisInterface class is single to the analysis.")
 
         cls._instance = super().__new__(cls)
         return cls._instance
@@ -95,3 +76,6 @@ class StructuralAnalysisInterface:
     @abstractmethod
     def __call__(self) -> Any:
         pass
+
+    @abstractmethod
+    def renumber_dofs(self) -> None: ...
